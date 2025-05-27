a7f40aa2bac05b64c5433ead6e5e6cc730bc9781
Chris Hayen <chayen@gmail.com>
2025-05-27 17:21:37 -0400

    hooked it up

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::path::PathBuf;
+
+mod discovery;
+mod util;
+
+pub async fn start(state_dir: Option<PathBuf>) -> Result<(), Box<dyn std::error::Error>> {
+    let discovery = discovery::Network::new(state_dir)?;
+    discovery.join().await?;
+
+    Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         // let result = add(2, 2);
+//         // assert_eq!(result, 4);
+//     }
+// }
