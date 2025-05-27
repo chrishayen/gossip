cc456cc2035cbb97feefb58953d5ea009d064ad5
Chris Hayen <chayen@gmail.com>
2025-05-27 17:21:42 -0400

    example

---
+++ examples/basic.rs
@@ -0,0 +1,24 @@
+use std::time::Duration;
+
+use gossip::start;
+use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() {
+    let num_nodes = 3;
+    let mut handles = Vec::new();
+
+    for _ in 0..num_nodes {
+        let tmp_dir = tempdir::TempDir::new("gossip-test").unwrap();
+        let handle = tokio::spawn(async move {
+            start(Some(tmp_dir.path().to_path_buf())).await.unwrap();
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
+
+    sleep(Duration::from_secs(10)).await;
+}
