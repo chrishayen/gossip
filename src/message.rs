044d32b8dcc9d1819b18481b488ce599704a8f89
Chris Hayen <chayen@gmail.com>
2025-05-28 13:01:42 -0400

    fixed Vec type

--- src/message.rs
+++ src/message.rs
@@ -1,17 +1,17 @@
+use crate::node::Node;
+use heapless::Vec;
+use postcard;
 use serde::{Deserialize, Serialize};
 
-use crate::node::{Node, NodeStatus};
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 pub enum GossipMessage {
     Heartbeat { sender_id: u32 },
     Update { node: Node },
-    Application { sender_id: u32, data: Vec<u8> }, // Custom app data
 }
 
 impl GossipMessage {
-    pub fn serialize(&self) -> Result<Vec<u8>, postcard::Error> {
-        postcard::to_allocvec(self)
+    pub fn serialize(&self) -> Result<Vec<u8, 1024>, postcard::Error> {
+        postcard::to_vec(self)
     }
 
     pub fn deserialize(data: &[u8]) -> Result<Self, postcard::Error> {
