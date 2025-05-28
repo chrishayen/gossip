90b0d9ea4a84f78bf4dd3e951deeeb6f20d14c08
Chris Hayen <chayen@gmail.com>
2025-05-28 13:01:26 -0400

    logger and mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
-use retry::retry;
-use std::{path::PathBuf, process::exit, time::Duration};
+mod config;
+mod message;
+mod node;
+mod protocol;
 mod retry;
 mod tailscale;
 mod util;
 
+use retry::retry;
+use std::{path::PathBuf, process::exit, time::Duration};
+
 pub async fn start(state_dir: Option<PathBuf>) -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
     let peer = tailscale::Peer::new(state_dir)?;
     let rs = retry(
         || peer.join_network(),
