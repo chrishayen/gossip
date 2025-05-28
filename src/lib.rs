d6eb6453d93d9c08afb53fc8ed44ca4cc68dd408
Chris Hayen <chayen@gmail.com>
2025-05-28 18:47:27 -0400

    send heartbeat and add peers

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,70 @@
 mod config;
 mod error;
-mod message;
+pub mod message;
 mod node;
 mod protocol;
 mod retry;
 mod tailscale;
 mod util;
-use log::error;
+
+use log::info;
+use node::Node;
+pub use protocol::GossipHandler;
 use retry::retry;
-use std::{path::PathBuf, process::exit, time::Duration};
+use std::{net::SocketAddr, path::PathBuf, time::Duration};
+use util::{extract_ipv4, hash_node_name};
 
-pub async fn start(state_dir: Option<PathBuf>) -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
-
-    let peer = tailscale::Peer::new(state_dir)?;
-    let rs = retry(
-        || peer.join_network(),
+pub async fn start(
+    handler: impl GossipHandler,
+    state_dir: Option<PathBuf>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let config = config::GossipConfig::default();
+    let ts = tailscale::Tailscale::new(state_dir)?;
+    let name = retry(
+        || ts.join_network(),
         Some(3),
         Some(Duration::from_secs(10)),
     )
-    .await;
+    .await?;
 
-    if rs.is_err() {
-        error!("Failed to join network: {:?}", rs.err());
-        exit(1)
+    info!("joined network {}", name);
+
+    let peers = retry(
+        || ts.get_peers(),
+        Some(3),
+        Some(Duration::from_secs(10)),
+    )
+    .await?;
+
+    info!("potential peers: {:?}", peers);
+
+    let ip4 =
+        retry(|| ts.get_ip(), Some(3), Some(Duration::from_secs(10)))
+            .await?;
+
+    let ip4 = extract_ipv4(&ip4)?;
+    let ip4 = SocketAddr::from((ip4, 42069));
+    info!("ip: {:?}", ip4);
+
+    let node_id = hash_node_name(&name);
+    let me = Node::new(node_id, ip4);
+
+    let udp_fd = ts.listen().await?;
+
+    let mut protocol = protocol::GossipProtocol::new(
+        me, udp_fd, config, handler, ts,
+    );
+
+    for peer in peers {
+        protocol.add_peer(peer);
     }
 
-    println!("Joined network {}", rs.unwrap());
-    peer.listen().await?;
+    protocol.send_heartbeat()?;
+    // protocol.gossip().await?;
+
+    // let handle = tokio::spawn(async move { peer.listen().await });
+
+    // info!("peers: {:?}", peers);
 
     Ok(())
 }
