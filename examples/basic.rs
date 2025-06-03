694b72f99675206d52a9e6f5da2c1242792473b0
Chris Hayen <chayen@gmail.com>
2025-06-03 16:45:29 -0400

    removed multiple node threading example

--- examples/basic.rs
+++ examples/basic.rs
@@ -5,42 +5,30 @@ use log::info;
 #[tokio::main]
 async fn main() {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
+    let tmp_dir = tempdir::TempDir::new("gossip-test").unwrap();
 
-    let num_nodes = 1;
-    let mut handles = Vec::new();
+    let mut gossip_config = GossipConfig::default();
 
-    for _ in 0..num_nodes {
-        let tmp_dir = tempdir::TempDir::new("gossip-test").unwrap();
+    // connect to tailscale
+    let mut ts = tailscale::Tailscale::new(
+        gossip_config.clone(),
+        Some(tmp_dir.path().to_path_buf()),
+    )
+    .unwrap();
 
-        let handle = tokio::spawn(async move {
-            let mut gossip_config = GossipConfig::default();
+    ts.join_network().await.unwrap();
+    ts.listen().await.unwrap();
 
-            // connect to tailscale
-            let mut ts = tailscale::Tailscale::new(
-                gossip_config.clone(),
-                Some(tmp_dir.path().to_path_buf()),
-            )
-            .unwrap();
-            ts.join_network().await.unwrap();
-            ts.listen().await.unwrap();
+    let seed_peers = ts.get_peers().await.unwrap();
+    let ip = ts.get_ip().await.unwrap();
+    let ip = util::extract_ipv4(ip.as_str()).unwrap();
 
-            let seed_peers = ts.get_peers().await.unwrap();
-            let ip = ts.get_ip().await.unwrap();
-            let ip = util::extract_ipv4(ip.as_str()).unwrap();
-            info!("ip: {}", ip);
+    info!("ip: {}", ip);
 
-            gossip_config.ip_address = ip.to_string();
-            gossip_config.node_name = ts.id.clone();
+    gossip_config.ip_address = ip.to_string();
+    gossip_config.node_name = ts.id.clone();
 
-            start(gossip_config, Box::new(ts), seed_peers)
-                .await
-                .unwrap();
-        });
-
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.await.unwrap();
-    }
+    start(gossip_config, Box::new(ts), seed_peers)
+        .await
+        .unwrap();
 }
