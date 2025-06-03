47d9235f49343581b8d5ac5235e4554e1d2f22d1
Chris Hayen <chayen@gmail.com>
2025-06-03 11:28:35 -0400

    more flexible message type

--- src/message.rs
+++ src/message.rs
@@ -1,12 +1,16 @@
+use std::fmt::Debug;
+
 use crate::node::Node;
 use heapless::Vec;
 use postcard;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
-pub enum GossipMessage {
-    Heartbeat { sender_id: u32 },
-    Update { node: Node },
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Clone)]
+pub struct GossipMessage {
+    pub from_id: u32,
+    pub ttl: u8,
+    pub msg_type: String,
+    pub payload: Vec<u8, 1024>,
 }
 
 impl GossipMessage {
@@ -16,5 +20,35 @@ impl GossipMessage {
 
     pub fn deserialize(data: &[u8]) -> Result<Self, postcard::Error> {
         postcard::from_bytes(data)
+    }
+
+    pub fn heartbeat(from_id: u32) -> GossipMessage {
+        GossipMessage {
+            from_id,
+            ttl: 3,
+            msg_type: "heartbeat".to_string(),
+            payload: postcard::to_vec("").unwrap(),
+        }
+    }
+
+    pub fn update(node: Node) -> GossipMessage {
+        GossipMessage {
+            from_id: node.id,
+            ttl: 1,
+            msg_type: "update".to_string(),
+            payload: postcard::to_vec(&node).unwrap(),
+        }
+    }
+
+    pub fn serialize_payload<T: Serialize>(
+        msg: T,
+    ) -> Result<Vec<u8, 1024>, postcard::Error> {
+        postcard::to_vec(&msg)
+    }
+
+    pub fn deserialize_payload<T: for<'de> Deserialize<'de>>(
+        msg: &[u8],
+    ) -> Result<T, postcard::Error> {
+        postcard::from_bytes(msg)
     }
 }
